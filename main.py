bedf03a4a2d727a7c6b2cf8b647f21265f23c90a
nithyarg <nithyaasbsc@gmail.com>
2024-04-20 18:49:20 +0530

    2.06m complted

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel 
 from random import randrange   
@@ -16,6 +16,16 @@ class post(BaseModel):
 my_posts= [{"title" :"title of post 1","content": "content of post 1", "id":1},
                 {"title" : "favorite foods", "content": "I like pizza","id":2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
+ 
+ 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i 
 
 @app.get("/")
 def read_root():
@@ -26,9 +36,34 @@ def get_posts():
     return {"data":my_posts} 
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,1000000) 
     my_posts.append(post_dict)
     return{"data": post_dict}
+
+
+
+ 
+@app.get("/posts/{id}")
+def get_post(id: int):
+   
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} was not found")
+    return{"post_detail": post} 
+
+@app.delete("/posts/{id}")
+def delete_post(id: int): 
+    # deleting post
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+    index = find_index_post(id)
+
+    my_posts.pop(index)
+    return {'message':'post was successfuly deleted'} 
+
+
+

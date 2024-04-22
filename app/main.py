7bfc2575e8be84e0b15899108fcca37d8c8f5e35
nithyarg <nithyaasbsc@gmail.com>
2024-04-22 23:32:29 +0530

    5hr:27m complted

--- app/main.py
+++ app/main.py
@@ -57,25 +57,35 @@ def read_root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    return{"status":"success"}
+
+    posts = db.query(models.post).all()
+    
+    print(posts)
+    return{"data": "successfull"}
 
 
 @app.get("/posts")
-def get_posts():
-    posts = cursor.execute("""SELECT * FROM posts """)
-    posts = cursor.fetchall()
+def get_posts(db: Session = Depends(get_db)):
+    # posts = cursor.execute("""SELECT * FROM posts """)
+    # posts = cursor.fetchall()
+    posts = db.query(models.post).all()
     return {"data": posts}  
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: post):
-    cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING 
-     * """,
-                    (post.title, post.content, post.published))
-    new_post =cursor.fetchone()
+def create_posts(post: post, db: Session = Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING 
+    #  * """,
+    #                 (post.title, post.content, post.published))
+    # new_post =cursor.fetchone()
 
-    conn.commit()
+    # conn.commit()
     
+    new_post = models.post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+
     return{"data": new_post}
     
 
@@ -83,9 +93,11 @@ def create_posts(post: post):
 
  
 @app.get("/posts/{id}")
-def get_post(id: int):
-    cursor.execute("""SELECT * from posts WHERE id = %s""",(str(id)))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute("""SELECT * from posts WHERE id = %s""",(str(id)))
+    # post = cursor.fetchone()
+    post = db.query(models.post).filter(models.post.id == id).first()
+   
    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -93,38 +105,46 @@ def get_post(id: int):
     return{"post_detail": post} 
 
 @app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int): 
+def delete_post(id: int, db: Session = Depends(get_db)): 
 
-    cursor.execute(
-        """DELETE FROM posts WHERE id = %s returning *""",(str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+    # cursor.execute(
+    #     """DELETE FROM posts WHERE id = %s returning *""",(str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.post).filter(models.post.id == id)
    
 
-    if delete_post == None:
+    if post.first()== None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
-
+    
+    post.delete(synchronize_session=False)
+    db.commit()
    
     return Response(status_code=status.HTTP_204_NO_CONTENT) 
 
 
 @app.put("/posts/{id}")
-def update_post(id: int, post: post):
+def update_post(id: int, post: post, db: Session = Depends(get_db)):
 
-    cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s  WHERE id = %s
-    RETURNING *""",
-                   (post.title, post.content, post.published, str(id)))
-    updated_post = cursor.fetchone()
-    conn.commit()
-   
+    # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s  WHERE id = %s
+    # RETURNING *""",
+    #                (post.title, post.content, post.published, str(id)))
+    # updated_post = cursor.fetchone()
+    # conn.commit()
+    post_query = db.query(models.post).filter(models.post.id == id)
 
-    if update_post == None:
+    post = post_query.first()   
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} does not exist")
     
+    post_query.update(post.dict(), synchronize_session=False)
+                       
+    db.commit()
     
-    return{"data": updated_post}
+    return {"data": post_query.first()}
 
 
 

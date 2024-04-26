0042e964b7df79e7cfbbc85b65344aa0f93eabfd
nithyarg <nithyaasbsc@gmail.com>
2024-04-26 06:59:40 +0530

    6hr:19m completed

--- app/post.py
+++ app/post.py
@@ -1,42 +1,3 @@
-from typing import Optional, List
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel 
-from passlib.context import CryptContext
-from random import randrange 
-import psycopg2 
-from psycopg2.extras import RealDictCursor 
-import time
-from sqlalchemy.orm import Session
-from . import models, schemas, utils
-from .database import engine, get_db
-
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-models.Base.metadata.create_all(bind=engine) 
-
-app = FastAPI()
-
-
-
-
-
-    
-while True:
-
-    try:    
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', 
-                            password='nithya96',cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was succesfull!")
-        break
-    except Exception as error:
-        print("Connecting to database failed")
-        print("Error:", error)
-        time.sleep(2)
-
-
 
 my_posts= [{"title" :"title of post 1","content": "content of post 1", "id":1},
                 {"title" : "favorite foods", "content": "I like pizza","id":2}]
@@ -152,16 +113,9 @@ def create_user( user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
-    return new_user
-
-@app.get('/users/{id}', response_model=schemas.UserOut)
-def get_user(id: int, db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                             detail=f"User with id:{id} does not exist")
-    
-    return user     
+    return new_user 
 
 
+@app.get('/users/{id}', reponse_model=schemas.UserOut)
+def get_user(id: int, db:Session = Depends(get_db), ):
+    user=db.query(models.User).filter(models.User.id == id).first()
